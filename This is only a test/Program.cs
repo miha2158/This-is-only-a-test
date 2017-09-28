380537c58dbf0c1908b01054dc0c41f31dbf0843
miha2158 <miha.vanya@gmail.com>
2017-09-29 00:29:00 +0500

    Add project files.

---
+++ This is only a test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This_is_only_a_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
